fe20cf151b5fd8e7b57fbfbfe03573ad5288351a
Hugo Legramandi <hugolegramandi@gmail.com>
2022-03-29 01:35:45 -0300

    Adição Modal

---
+++ assets/js/script.js
@@ -0,0 +1,102 @@
+function fechar1(){
+    document.getElementById('botao1').style.display = 'none';
+    window.location.href = "#dados";
+
+}
+function abrir1(){
+    document.getElementById('botao1').style.display = 'block';
+}
+
+function fechar2(){
+    document.getElementById('botao2').style.display = 'none';
+    window.location.href = "#dados";
+}
+
+function abrir2(){
+    document.getElementById('botao2').style.display = 'block';
+}
+
+function fechar3(){
+    document.getElementById('botao3').style.display = 'none';
+    window.location.href = "#dados";
+}
+function abrir3(){
+    document.getElementById('botao3').style.display = 'block';
+}
+
+function fechar4(){
+    document.getElementById('botao4').style.display = 'none';
+    window.location.href = "#dados";
+}
+
+function abrir4(){
+    document.getElementById('botao4').style.display = 'block';
+}
+
+function fechar5(){
+    document.getElementById('botao5').style.display = 'none';
+    window.location.href = "#dados";
+}
+function abrir5(){
+    document.getElementById('botao5').style.display = 'block';
+}
+
+function fechar6(){
+    document.getElementById('botao6').style.display = 'none';
+    window.location.href = "#dados";
+}
+
+function abrir6(){
+    document.getElementById('botao6').style.display = 'block';
+}
+
+function fechar7(){
+    document.getElementById('botao7').style.display = 'none';
+    window.location.href = "#dados";
+}
+function abrir7(){
+    document.getElementById('botao7').style.display = 'block';
+}
+
+function fechar8(){
+    document.getElementById('botao8').style.display = 'none';
+    window.location.href = "#dados";
+}
+
+function abrir8(){
+    document.getElementById('botao8').style.display = 'block';
+}
+
+function fechar9(){
+    document.getElementById('botao9').style.display = 'none';
+    window.location.href = "#dados";
+}
+function abrir9(){
+    document.getElementById('botao9').style.display = 'block';
+}
+
+function fechar10(){
+    document.getElementById('botao10').style.display = 'none';
+    window.location.href = "#dados";
+}
+
+function abrir10(){
+    document.getElementById('botao10').style.display = 'block';
+}
+
+function fechar11(){
+    document.getElementById('botao11').style.display = 'none';
+    window.location.href = "#dados";
+}
+function abrir11(){
+    document.getElementById('botao11').style.display = 'block';
+}
+
+function fechar12(){
+    document.getElementById('botao12').style.display = 'none';
+    window.location.href = "#dados";
+}
+
+function abrir12(){
+    document.getElementById('botao12').style.display = 'block';
+}
